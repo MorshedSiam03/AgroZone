1cd71e80896d432fb5820335b2e50aba82f70ebb
Morshed Siam <morshedsiam33@gmail.com>
2024-11-04 08:37:04 +0600

    Add Structure

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -7,6 +7,7 @@ export default function Root() {
     <>
       <Navbar/>
       <h1>Welcome to AgroZone</h1> {/* Demo text, Remove it when you work on the project */}
+      <p>Kamil</p> {/* Demo text, Remove it when you work on the project */}
       <Footer/>
     </>
   )
