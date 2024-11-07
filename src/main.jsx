b6f3aa0a4d0e7d4de91621d7f57c58d9018ad922
Kamil-Ahmed67 <nabilahmedtalukder6272@gmail.com>
2024-11-07 20:45:50 +0600

    Error Page Added

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root/>,
-    errorElement: <ErrorPage/>,
+    errorElement:<ErrorPage></ErrorPage>,
     children: [
       {
         path: "/contacts",
