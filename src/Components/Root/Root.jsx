fea9c93bd2caffdf28133564242748b6e8b27190
Morshed Siam <morshedsiam33@gmail.com>
2024-11-08 08:54:31 +0600

    Product

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 export default function Root() {
   return (
     <>
       <Navbar/>
+      <Outlet/>
       <Footer/>
     </>
   )
